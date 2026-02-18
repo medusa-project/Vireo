e3e344d4cc0861090f36ab08e793fc1fd7cfe438
srbbins <srobbins@illinois.edu>
2026-02-17 18:10:04 -0600

    update degrees-granted to work with v4 db

--- bin/degrees-granted-report/run-report.js
+++ bin/degrees-granted-report/run-report.js
@@ -19,7 +19,7 @@ let handle_db_err=function(err){
 
 let handle_db_results=function(results){
     let submissionRows = new SubmissionList(results);
-    console.log(submissionRows);
+    console.log(submissionRows.rows);
     stringify(submissionRows.rows, {
         header: true,
         quote: "~",
@@ -42,11 +42,11 @@ client.connect();
 
 let results=[];
 //TODO make date range flexible
-let sub_query_promise = client.query('select * from submission where approvaldate between current_date-30 and current_date');
-let committee_memb_promise=client.query('select * from committee_member left outer join committee_member_roles on committee_member.id=committee_member_roles.jpacommitteememberimpl_id');
-let embargo_type_promise=client.query('select id, name from embargo_type');
+// let sub_query_promise = client.query('select * from submission where approvaldate between current_date-30 and current_date');
+// let committee_memb_promise=client.query('select * from committee_member left outer join committee_member_roles on committee_member.id=committee_member_roles.jpacommitteememberimpl_id');
+// let embargo_type_promise=client.query('select id, name from embargo_type');
 
-sub_query_promise.then(result => {
+/* sub_query_promise.then(result => {
     results[0]=result;
     console.log(result);
     committee_memb_promise.then(result => {
@@ -57,5 +57,40 @@ sub_query_promise.then(result => {
             client.end();
         }).catch(err => {handle_db_err(err)});
     }).catch(err => {handle_db_err(err)})
-}).catch(err => {handle_db_err(err)});
+}).catch(err => {handle_db_err(err)}); */
 
+let test_promise = client.query(`
+    SELECT json_build_object(
+  'submission', to_jsonb(s),
+  'field_value', json_build_object(
+      'id', fv.id,
+      'value', fv.value,
+      'identifier', fv.identifier,
+      'definition', fv.definition,
+      'field_predicate_id', fv.field_predicate_id
+  ),
+  'field_predicate', json_build_object(
+      'id', fp.id,
+      'value', fp.value,
+      'document_type_predicate', fp.document_type_predicate
+  ),
+  'submission_field_values', json_build_object(
+      'submission_id', sfv.submission_id,
+      'field_values_id', sfv.field_values_id
+  )
+) AS row
+FROM submission               AS s
+JOIN submission_field_values  AS sfv ON s.id = sfv.submission_id
+JOIN field_value              AS fv  ON fv.id = sfv.field_values_id
+JOIN field_predicate          AS fp  ON fp.id = fv.field_predicate_id
+WHERE s.approve_application_date BETWEEN current_date - INTERVAL '30 days' AND current_date;
+`);
+
+test_promise.then(result => { console.log(result);
+    // handle_db_results([result]);
+console.log("############### Contents are ###############")
+
+
+    client.end();
+    handle_db_results(result);
+}).catch(err => {handle_db_err(err)});
--- bin/degrees-granted-report/submission_list.js
+++ bin/degrees-granted-report/submission_list.js
@@ -1,132 +1,185 @@
 var datefns = require('date-fns');
 
 class SubmissionList {
-    constructor(db_results)
+    constructor(db_result)
     {
-        this.month_names=["January","February","March","April","May","June","July",
-            "August","September","October","November","December"];
-        let sub_rows=db_results[0]["rows"];
-        let embargo_rows=db_results[2]["rows"];
-        let committee_member_json=this.build_committee_json(db_results[1]["rows"]);
+        
+        // let embargo_rows=db_results[2]["rows"];
+        // let committee_member_json=this.build_committee_json(db_results[1]["rows"]);
+        this.header=["UIN","First Name","Last Name","Middle Name","Degree Level",
+            "Degree Name","Degree Department","Department Code","Program","Program Code",
+            "Major Name","Embargo Option","Title","Deposit Date","Degree Month","Degree Year",
+            "Chair1","DirectorResearch1","CommitteeMbr1","CommitteeMbr2","Chair2","Chair3",
+            "Chair4","Chair5","Advisor1","Advisor2","Advisor3","Advisor4","DirectorResearch2",
+            "DirectorResearch3","DirectorResearch4","CommitteeMbr3","CommitteeMbr4","CommitteeMbr5",
+            "CommitteeMbr6","CommitteeMbr7","CommitteeMbr8","CommitteeMbr9","CommitteeMbr10"];
+
+        this.embargo_map = {
+        "Open Access": "Open",
+        "U of I Access": "Campus",
+        "Closed Access": "Close"
+        }
+
+        this.organization_map =
+        {  
+            2: "Dissertation", 
+            3: "Thesis"
+        }
+        this.row_limits={
+            "Chair":5,
+            "DirectorResearch":4,
+            "CommitteeMbr":10,
+            "Advisor":4
+        }
+        
+
         this.rows=[];
-        for (let row of sub_rows){
+        this.submission_metadata_map={}
+
+        for (const r of db_result.rows){
+            
+            let predicate=r.row["field_predicate"]["value"];
+            let field_value=r.row["field_value"]["value"];
+            let submission_id=r.row["submission"]["id"];
+    
+            this.submission_metadata_map[submission_id]??={};
+            this.submission_metadata_map[submission_id]["submission_date"]??=r.row["submission"]["submission_date"];
+            this.submission_metadata_map[submission_id][predicate]??=[];
+            this.submission_metadata_map[submission_id][predicate].push(field_value);
+            this.submission_metadata_map[submission_id]["documenttype"]??=r.row["field_predicate"]["document_type_predicate"];
+            this.submission_metadata_map[submission_id]["organization_id"]??=r.row["submission"]["organization_id"];
+
+
+
+        
+        }
+        this.build_rows();
+    }
+
+    build_rows(){
+        for (const submission_id in this.submission_metadata_map){
             let sub_info={};
-            sub_info["UIN"] = row["uin"]||"";
-            sub_info["First Name"] = row["studentfirstname"]||"";
-            sub_info["Last Name"] = row["studentlastname"]||"";
-            sub_info["Middle Name"] = row["studentmiddlename"]||"";
+            let metadata=this.submission_metadata_map[submission_id];
+            sub_info["UIN"] = metadata["uin"]??"";
+            sub_info["First Name"] = metadata["first_name"]??"";
+            sub_info["Last Name"] = metadata["last_name"]??"";
+            sub_info["Middle Name"] = metadata["middle_name"]??"";
+            sub_info["Degree Level"] = this.get_degree_level(metadata);
+            sub_info["Degree Name"] = metadata["thesis.degree.name"]??"";
+            sub_info["Degree Department"] = metadata["thesis.degree.department"]??"";
+            sub_info["Department Code"] = metadata["department_code"]??"";
+            sub_info["Program"] = metadata["degree_code"]??"";
+            sub_info["Program Code"] = metadata["major_code"]??"";
+            sub_info["Major Name"] = metadata["thesis.degree.major"]??"";
+            sub_info["Embargo Options"] = this.get_embargo_options(metadata);
+            sub_info["Title"] = metadata["dc.title"]??"";
+            sub_info["Deposit Date"] = metadata["submission_date"]??"";
+            sub_info["Degree Month"] = this.get_degree_month(metadata);
+            sub_info["Degree Year"] = this.get_degree_year(metadata);
+            sub_info  = this.build_committee_info(sub_info, metadata);
+            
+        
 
-            //what was degree level in vireo 1 is now document type
-
-            sub_info["Degree Level"] = row["documenttype"]||"";
-            sub_info["Degree Name"] = row["degree"]||"";
-            sub_info["Degree Department"] = row["department"]||"";
-            sub_info["Department Code"] = row["departmentcode"]||"";
-            sub_info["Program"] = row["program"]||"";
-            sub_info["Program Code"] = row["programcode"]||"";
-            sub_info["Major Name"] = row["major"]||"";
-            sub_info["Embargo Option"] = this.resolve_embargo_type(row["embargotype_id"], embargo_rows);
-            sub_info["Title"] = row["documenttitle"]||"";
-            sub_info["Deposit Date"] = this.resolve_date(row["approvaldate"]);
-            sub_info["Degree Month"] = this.month_names[row["graduationmonth"]]||"";
-            sub_info["Degree Year"] = row["graduationyear"]||"";
-
-
-            this.add_committee_info(sub_info, committee_member_json, row.id);
-            this.add_empty_rows(sub_info);
-
-
-            this.rows.push(sub_info);
-        }
-    }
-
-    get first_names() {
-        let first_name_array=[];
-
-        for (let row of this.rows){
-            first_name_array.push(row.first_name);
-        }
-        return first_name_array;
-    }
-    get first_submission() {
-        return this.rows[0];
-    }
-
-    build_committee_json(committee_rows){
-        let committee_member_json = {};
-
-        for (let row of committee_rows){
-            if (committee_member_json[row['submission_id']]===undefined){
-                committee_member_json[row['submission_id']]=[];
-            }
-            committee_member_json[row['submission_id']].push(row);
-        }
-        return committee_member_json;
-    }
-    add_committee_info(sub_info, committee_member_json, sub_id){
-        let adviser_counter=1;
-        let committee_member_counter=1;
-        let chair_counter=1;
-        let dor_counter=1;
-
-        for (let row of committee_member_json[sub_id]){
-            if (row["roles"]===null){
-                sub_info["CommitteeMbr"+committee_member_counter]=this.format_committee_name(row);
-                committee_member_counter++;
-            } else if (row["roles"]==='Director of Research'){
-                sub_info["DirectorResearch"+dor_counter]=this.format_committee_name(row);
-                dor_counter++;
-            } else if (row["roles"]==='Chair' || row["roles"]==="Committee Chair"){
-                sub_info["Chair"+chair_counter]=this.format_committee_name(row);
-                chair_counter++;
-            } else if (row["roles"]==='Advisor' || row["roles"]==="Adviser"){
-                sub_info["Advisor"+adviser_counter]=this.format_committee_name(row);
-                adviser_counter++;
-            } else {
-                throw "unexpected committee value: "+row.toString()
-            }
-        }
-
-    }
-
-    add_empty_rows(sub_info){
-        let committee_role_quantity = {'Chair': 5, 'Advisor': 4, 'DirectorResearch': 4, 'CommitteeMbr': 10};
-        for (let committee_role in committee_role_quantity){
-            for(let i=1; i<=committee_role_quantity[committee_role]; i++){
-                if (!Object.keys(sub_info).includes(committee_role+i) ){
-                    sub_info[committee_role+i]="";
+           
+            //collapse arrays into || separated strings, empty string if array is empty.
+            for (const [key, value] of Object.entries(sub_info)) {
+                if (Array.isArray(value)) {
+                  sub_info[key] = value.length === 0 ? "" : value.join("||");
                 }
             }
+            
+            this.rows.push(sub_info);
+
+
         }
     }
-
-    format_committee_name(row){
-        let lastname = row.lastname || "";
-        let firstname = row.firstname || "";
-        let middlename = row.middlename || "";
-        return lastname+", "+firstname+" "+middlename;
-    }
-
-    resolve_embargo_type(db_embargo_id, db_embago_rows) {
-
-        let embargo_type_map=new Map();
-        for (let row of db_embago_rows){
-            embargo_type_map.set(row.id, row.name)
+    get_degree_level(metadata)
+    {
+        if (metadata["organization_id"]){
+            return this.organization_map[metadata["organization_id"]];
+        } else {
+        return "";
         }
-        if (db_embargo_id===undefined || db_embargo_id==="" || embargo_type_map.get(db_embargo_id)==="Open Access"){
-            return "Open";
-        } else if (embargo_type_map.get(db_embargo_id)==="U of I Access"){
-            return "Campus";
-        } else if (embargo_type_map.get(db_embargo_id)==="Closed Access"){
-            return "Close";
-        } else{
-            throw "Unexpected embargo info: "+db_embargo_id
-        }
+    }
+    get_embargo_options(metadata)
+    {
+       if (metadata["default_embargos"]){
+        return this.embargo_map[metadata["default_embargos"]];
+        
+        } else { 
+            return this.embargo_map[metadata["Open Access"]];
+       }
+    }
+    
+    get_degree_month(metadata)
+    {
 
+        if (metadata["dc.date.issued"]){
+            return metadata["dc.date.issued"][0].split(" ")[0];
+
+        } else {
+            return "";
+        }  
+    }    
+    get_degree_year(metadata)
+    {
+
+        if (metadata["dc.date.issued"]){
+            return metadata["dc.date.issued"][0].split(" ")[1];
+
+        } else {
+            return "";
+        }
+    } 
+    build_committee_info(sub_info, metadata){
+        let dor_arr=[];
+        let chair_arr=[];
+        let mem_arr=[];
+        let advisor_arr=[];
+        console.log(metadata["organization_id"]);
+
+        if (metadata["organization_id"]===2){
+            console.log(metadata["dc.contributor.advisor"]);
+            dor_arr=this.ensure_array(metadata["dc.contributor.advisor"]);
+            if (metadata["co-director_of_research"]){
+                dor_arr.push(...metadata["co-director_of_research"]);
+            }
+            chair_arr=this.ensure_array(metadata["dc.contributor.committeeChair"]);
+            mem_arr=this.ensure_array(metadata["dc.contributor.committeeMember"]);
+            
+            
+        } else if (metadata["organization_id"]===3){    
+
+            advisor_arr=this.ensure_array(metadata["dc.contributor.advisor"]);            
+            mem_arr=this.ensure_array(metadata["dc.contributor.committeeMember"]);  
+        }
+        this.add_committee_info(sub_info, dor_arr, "DirectorResearch");
+        this.add_committee_info(sub_info, chair_arr, "Chair");
+        this.add_committee_info(sub_info, mem_arr, "CommitteeMbr"); 
+        this.add_committee_info(sub_info, advisor_arr, "Advisor");
+        return sub_info;
     }
-    resolve_date(db_date){
-        return datefns.formatISO(db_date, { representation: 'date' });
-    }
+   
+    add_committee_info(sub_info, committee_arr, prefix){
+        let limit = this.row_limits[prefix];
+        console.log("################# got here #################");
+        console.log(limit);
+        if (Array.isArray(committee_arr)){
+            while (committee_arr.length<limit){
+                committee_arr.push(""); 
+            }
+            console.log(committee_arr);
+            committee_arr.forEach((member, i) => {
+                    sub_info[prefix+(i+1)]=member;
+            });
+        }
+        
+        return sub_info;
+    } 
+    ensure_array(v){
+        return Array.isArray(v) ? v : (v == null ? [] : [v]);
+    } 
+
 }
 
 module.exports=SubmissionList;
